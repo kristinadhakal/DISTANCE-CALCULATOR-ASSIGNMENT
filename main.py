19cff530b476c64f0fa31592bed6944fa748cafc
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2023-01-31 20:51:11 -0700

    jan 31

---
+++ main.py
@@ -0,0 +1,13 @@
+# input
+
+x1 = float(input("Enter  x1 value:"))
+x2 = float(input("Enter  x2 value:"))
+y1 = float(input("Enter  y1 value:"))
+y2 = float(input("Enter  y2 value:"))
+
+# process
+
+distance = ((x2 - x1)** 2 + (y2 - y1)** 2) ** 0.5
+# output 
+
+print("the distance is " + str(distance) + ".")
